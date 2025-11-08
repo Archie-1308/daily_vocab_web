7a40b335be859b4305627c0f9034f473d62af3e9
Archie <archana7941@gmail.com>
2025-11-08 14:32:10 +0700

    update leaw yay

--- src/app/api/word/route.ts
+++ src/app/api/word/route.ts
@@ -5,5 +5,10 @@ export async function GET() {
   const randomIndex = Math.floor(Math.random() * words.length);
   const word = words[randomIndex];
   console.log('Archie was here...');  
+
+ 
+
+  const response = await fetch('http://localhost:8000/api/word');
+  const data = await response.json();
   return NextResponse.json({ data: word });
 }
